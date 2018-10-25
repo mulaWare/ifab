841da9bd087c6c7c096c0658b0d4283424eacd22
Luis Vazquez <luis.vzqz@mulaware.com>
2018-10-25 11:40:34 -0500

    Update __manifest__.py

--- ifab/__manifest__.py
+++ ifab/__manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "luisvzqz",
+    'website': "http://www.mulaware.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','account','l10n_mx_edi',],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/payments.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
