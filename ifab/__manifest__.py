cc6489743367b7000f3061b16996d179b936d5b3
Luis Vazquez <luis.vzqz@mulaware.com>
2018-10-25 12:01:48 -0500

    Update __manifest__.py

--- ifab/__manifest__.py
+++ ifab/__manifest__.py
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+        'depends': ['base','account','l10n_mx_edi',],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/payments.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
