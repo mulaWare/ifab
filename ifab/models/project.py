eaa844df0719f47ca0f83970e2bc39303b8d898f
luisvzqz <luis.vzqz@mulaware.com>
2018-11-20 16:14:36 -0600

    a

--- ifab/models/project.py
+++ ifab/models/project.py
@@ -5,9 +5,9 @@ from datetime import timedelta
 
 class Project(models.Model):
     _inherit = 'project.project'
-    
+
     description = fields.Char("Description", default=False, help="Description of the project")
     contract = fields.Char("Contract", default=False, help="Contract of the project")
     study = fields.Char("Study", default=False, help="Study of the project")
     sample = fields.Integer("Sample", default=False, help="Size of the sample of the project")
-    date_sign = field.Date("Signature", default=False, help="Signature date of the contract")
+    date_sign = fields.Date("Signature", default=False, help="Signature date of the contract")
