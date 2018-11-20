2e81427e379f7f7452651dba16fc7db76b947f67
luisvzqz <luis.vzqz@mulaware.com>
2018-11-20 14:39:34 -0600

    project.py

--- ifab/models/project.py
+++ ifab/models/project.py
@@ -8,7 +8,6 @@ class Project(models.Model):
 
     description = fields.Char("Description", default=False, help="Description of the project")
     contract = fields.Char("Contract", default=False, help="Contract of the project")
-
+    study = fields.Char("Study", default=False, help="Study of the project")
     sample = fields.Integer("Sample", default=False, help="Size of the sample of the project")
     date_sign = field.Date("Signature", default=False, help="Signature date of the contract")
-    code = fields.Char('Order Reference', required=True, index=True, copy=False, default='New')
