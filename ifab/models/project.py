2cfab7ff6a0675f19dbaadff4897ae86897b996e
luisvzqz <luis.vzqz@mulaware.com>
2018-11-20 17:47:42 -0600

    Create

--- ifab/models/project.py
+++ ifab/models/project.py
@@ -10,4 +10,11 @@ class Project(models.Model):
     contract = fields.Char("Contract", default=False, help="Contract of the project")
     study = fields.Char("Study", default=False, help="Study of the project")
     sample = fields.Integer("Sample", default=False, help="Size of the sample of the project")
-    date_sign = fields.Date("Signature", default=False, help="Signature date of the contract")
+    date_sign = fields.Date("Signature Date", default=False, help="Signature date of the contract")
+    code = fields.Char(string='Reference', index=True, track_visibility='onchange', default='0000',readonly=True)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('code', '0000') == '0000':
+            vals['code'] = self.env['ir.sequence'].next_by_code('project.project') or '/'
+        return super(Project, self).create(vals)
