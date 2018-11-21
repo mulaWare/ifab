8a10e11abc28a0673b39118f4f5bf04f09e4b3d3
luisvzqz <luis.vzqz@mulaware.com>
2018-11-20 23:43:16 -0600

    pr3

--- ifab/models/project.py
+++ ifab/models/project.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 class Project(models.Model):
     _inherit = 'project.project'
 
+
     description = fields.Char("Description", default=False, help="Description of the project")
     contract = fields.Char("Contract", default=False, help="Contract of the project")
     study = fields.Char("Study", default=False, help="Study of the project")
