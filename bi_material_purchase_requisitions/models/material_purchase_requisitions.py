a80f14fd41173b50d65cdbfbc8f0fd4bbb7fa097
Luis Vazquez <luis.vzqz@mulaware.com>
2018-11-28 16:29:57 -0600

    Update material_purchase_requisitions.py

--- bi_material_purchase_requisitions/models/material_purchase_requisitions.py
+++ bi_material_purchase_requisitions/models/material_purchase_requisitions.py
@@ -139,13 +139,12 @@ class MaterialPurchaseRequisition(models.Model):
                                         'name' : line.description,
                                         'price_unit' : line.product_id.list_price,
                                         'account_analytic_id' : line.account_analytic_id.id,
-                                        'analytic_tag_ids' : line.analytic_tag_ids.ids,
+                                        'analytic_tag_ids' : [(6,0,[line.analytic_tag_ids.ids])],
                                         'date_planned' : datetime.now(),
                                         'product_uom' : line.uom_id.id,
                                         'order_id' : pur_order.id,
                         }
                         purchase_order_line = purchase_order_line_obj.sudo().create(po_line_vals)
-                        purchase_order_line.write({'analytic_tag_ids' : line.analytic_tag_ids.ids,})
                        
                         
                     else:
@@ -162,13 +161,12 @@ class MaterialPurchaseRequisition(models.Model):
                                         'name' : line.description,
                                         'price_unit' : line.product_id.list_price,
                                         'account_analytic_id' : line.account_analytic_id.id,
-                                        'analytic_tag_ids' : line.analytic_tag_ids.ids,                            
+                                        'analytic_tag_ids' : [(6,0,[line.analytic_tag_ids.ids])],                            
                                         'date_planned' : datetime.now(),
                                         'product_uom' : line.uom_id.id,
                                         'order_id' : purchase_order.id,
                         }
-                        purchase_order_line = purchase_order_line_obj.sudo().create(po_line_vals)
-                        purchase_order_line.write({'analytic_tag_ids' : line.analytic_tag_ids.ids,})                           
+                        purchase_order_line = purchase_order_line_obj.sudo().create(po_line_vals)                         
             else:
                 for vendor in line.vendor_id:
                     stock_picking_obj = self.env['stock.picking']
