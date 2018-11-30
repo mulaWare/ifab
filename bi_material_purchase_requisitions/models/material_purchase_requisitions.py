164933d341fb04b3c7acf45faa2f776df202d715
luisvzqz <luis.vzqz@mulaware.com>
2018-11-30 12:09:55 -0600

    a2

--- bi_material_purchase_requisitions/models/material_purchase_requisitions.py
+++ bi_material_purchase_requisitions/models/material_purchase_requisitions.py
@@ -336,7 +336,7 @@ class RequisitionLine(models.Model):
     account_analytic_id = fields.Many2one('account.analytic.account', string='Analytic Account')
     analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic Tags')
     qty_available = fields.Float(string="Qty Available",related='product_id.qty_available',readonly=True)
-    location_id = fields.Many2one('stock.location', string='Location', auto_join=True, ondelete='restrict', readonly=True,  default=lambda self: self.env['stock.quant'].search(["&",('product_id', '=', self.product_id.id),('location_id.usage','=', 'internal')], limit=1))
+    location_id = fields.Many2one('stock.location', string='Location', auto_join=True, ondelete='restrict', readonly=True,  default=lambda self: self.env['stock.quant'].search(['&',('product_id', '=', self.product_id.id),('location_id.usage','=', 'internal')], limit=1))
 
 
 class StockPicking(models.Model):
