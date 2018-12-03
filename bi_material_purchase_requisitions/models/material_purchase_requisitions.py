85fcb6891615d8aaf001bca4d066a34a1d6bd32c
luisvzqz <luis.vzqz@mulaware.com>
2018-12-02 18:12:22 -0600

    q4

--- bi_material_purchase_requisitions/models/material_purchase_requisitions.py
+++ bi_material_purchase_requisitions/models/material_purchase_requisitions.py
@@ -333,7 +333,7 @@ class RequisitionLine(models.Model):
         if not self.qty_available:
             return res
         quant_id = self.env['stock.quant'].search(
-                    [('product_id', '=', self.product_id )],
+                    [('product_id', '=', self.product_id.id )],
                     limit=1)
         location = quant_id.location_id
         return location
