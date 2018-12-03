e70ce06cec31707fb22f2a3aa0df9ae8dfff6b24
luisvzqz <luis.vzqz@mulaware.com>
2018-12-03 08:58:33 -0600

    m1

--- bi_material_purchase_requisitions/models/material_purchase_requisitions.py
+++ bi_material_purchase_requisitions/models/material_purchase_requisitions.py
@@ -326,15 +326,6 @@ class RequisitionLine(models.Model):
         self.account_analytic_id = self.requisition_id.account_analytic_id.id
         self.analytic_tag_ids = self.requisition_id.analytic_tag_ids.ids
 
-    @api.multi
-    @api.onchange('qty_available')
-    def compute_location(self):
-        res = {}
-        if not self.qty_available:
-            return res
-        location = self.product_id.stock_quant_ids[1].location_id.id
-        return location
-
     product_id = fields.Many2one('product.product', string="Product")
     description = fields.Text(string="Description")
     qty = fields.Float(string="Quantity",default=1.0)
@@ -345,7 +336,7 @@ class RequisitionLine(models.Model):
     account_analytic_id = fields.Many2one('account.analytic.account', string='Analytic Account')
     analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic Tags')
     qty_available = fields.Float(string="Qty Available",related='product_id.qty_available',readonly=True)
-    location_id = fields.Many2one('stock.location', string='Location', auto_join=True, ondelete='restrict', readonly=True, )
+    location_id = fields.Many2one('stock.location', string='Location', auto_join=True, ondelete='restrict', readonly=True, required=True,related='product_id.stock_quant_ids.location_id')
 
 
 class StockPicking(models.Model):
