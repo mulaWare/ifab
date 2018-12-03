a10a673a416d4c6784e5d0b44994a061cedf770f
luisvzqz <luis.vzqz@mulaware.com>
2018-12-03 12:20:54 -0600

    a3

--- bi_material_purchase_requisitions/models/material_purchase_requisitions.py
+++ bi_material_purchase_requisitions/models/material_purchase_requisitions.py
@@ -325,7 +325,7 @@ class RequisitionLine(models.Model):
         self.description = self.product_id.name
         self.account_analytic_id = self.requisition_id.account_analytic_id.id
         self.analytic_tag_ids = self.requisition_id.analytic_tag_ids.ids
-        self.location_id = self.mapped('product_id.stock_quant_ids').filtered(lambda u: u.product_id.stock_quant_ids.location_id.usage = 'internal').browse()
+
 
     product_id = fields.Many2one('product.product', string="Product")
     description = fields.Text(string="Description")
@@ -337,7 +337,7 @@ class RequisitionLine(models.Model):
     account_analytic_id = fields.Many2one('account.analytic.account', string='Analytic Account')
     analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic Tags')
     qty_available = fields.Float(string="Qty Available",related='product_id.qty_available',readonly=True)
-    location_id = fields.Many2one('stock.location', string='Location', auto_join=True, ondelete='restrict', readonly=True, required=True)
+    location_id = fields.Many2one('stock.location', string='Location', auto_join=True, ondelete='restrict', readonly=True, required=True,default=lambda self: self.env['stock.inventory'].search(['&',('product_id','=',self.product_id.id),('usage','=', 'internal')], limit=1)
 
 
 class StockPicking(models.Model):
