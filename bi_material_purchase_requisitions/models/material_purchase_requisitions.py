ff1a43353fc355a54ab504163ea278a867a0afcc
luisvzqz <luis.vzqz@mulaware.com>
2018-12-04 15:24:03 -0600

    a5

--- bi_material_purchase_requisitions/models/material_purchase_requisitions.py
+++ bi_material_purchase_requisitions/models/material_purchase_requisitions.py
@@ -330,9 +330,9 @@ class RequisitionLine(models.Model):
 
         for record in self:
             if record.requisition_action == 'internal_picking':
-                record['vendor_id']= [(4, self.env['material.purchase.requisition'].company_id.partner_id.id)]
+                record.vendor_id = self.env['material.purchase.requisition'].company_id.partner_id.id)
             if record.requisition_action == 'purchase_order':
-                record['vendor_id']= [(4, x) for x in record.product_id.seller_ids.ids]
+                record.vendor_id = record.product_id.seller_ids.ids
 
 
 
