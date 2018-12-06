6de8d1853ab52cbfbbb923ad1b4aff2286b60451
luisvzqz <luis.vzqz@mulaware.com>
2018-12-06 09:03:42 -0600

    5

--- ifab/__manifest__.py
+++ ifab/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-        'depends': ['base','account','l10n_mx_edi','project','purchase',],
+        'depends': ['base','account','l10n_mx_edi','project','purchase','purchase_requisition'],
 
     # always loaded
     'data': [
