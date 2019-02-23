eab26408a1c830bbcf06b2160aac085eeaf2000e
luisvzqz <luis.vzqz@mulaware.com>
2019-02-22 23:23:21 -0600

    2

--- ifab/models/product.py
+++ ifab/models/product.py
@@ -35,3 +35,37 @@ class ProductProduct(models.Model):
     _sql_constraints = [
         ('barcode_uniq', 'unique(barcode)', "A barcode can only be assigned to one product !"),
     ]
+
+
+
+
+
+class ProductTemplate(models.Model):
+    _name = "product.template"
+    _inherit = "product.template"
+
+
+    # related to display product product information if is_product_variant
+    barcode = fields.Char('Barcode', oldname='ean13', related='product_variant_ids.barcode', readonly=False)
+    default_code = fields.Char(
+        'Internal Reference', compute='_compute_default_code',
+        inverse='_set_default_code', readonly=True, copy=False, store=True)
+
+
+    _sql_constraints = [
+        ('barcode_uniq', 'unique(barcode)', "A barcode can only be assigned to one product !"),
+    ]
+
+
+    @api.depends('product_variant_ids', 'product_variant_ids.default_code')
+    def _compute_default_code(self):
+        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+        for template in unique_variants:
+            template.default_code = template.product_variant_ids.default_code
+        for template in (self - unique_variants):
+            template.default_code = ''
+
+    @api.one
+    def _set_default_code(self):
+        if len(self.product_variant_ids) == 1:
+            self.product_variant_ids.default_code = self.default_code
